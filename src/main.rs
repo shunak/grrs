155704f37fa94981a6135a04c56833694b8cc803
shunak <shuna966@gmail.com>
2022-05-13 21:55:43 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![allow(unused)]
+
+use clap::Parser;
+use std::io::BufReader;
+use std::fs::File;
+
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines(){
+        if line.contains(&args.pattern){
+            println!("{}", line);
+        }
+    }
+
 }
