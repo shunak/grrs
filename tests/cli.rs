7f8ad3d800fe6b68fe956514c064feb51666c51b
shunak <shuna966@gmail.com>
2022-05-16 15:53:48 +0900

    Implement testing

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,8 @@
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
 use std::process::Command;
+use assert_fs::prelude::*;
+
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,3 +15,19 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 
 }
+
+#[test]
+fn file_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\n More content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+        cmd.arg("test").arg(file.path());
+    cmd.assert().success().stdout(predicate::str::contains("test\nAnother test"));
+
+    Ok(())
+}
+
+
+
+
